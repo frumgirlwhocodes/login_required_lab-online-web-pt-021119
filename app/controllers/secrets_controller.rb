66b077858684be187500722a359a044451061aea
lieby weintraub <liebahinda@gmail.com>
2019-08-05 04:11:46 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -4,5 +4,12 @@ class SecretsController < ApplicationController
   def show 
     
   end 
+    private
+  def require_login
+    unless session.include? :name
+      redirect_to :login
+    end
+  end
+end
   
 end 
