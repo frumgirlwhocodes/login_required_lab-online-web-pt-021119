42572d10c2f52ed68f7ec17e977386b3fd055154
lieby weintraub <liebahinda@gmail.com>
2019-08-05 04:30:22 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  post '/logout' => 'sessions#destroy'
-  get '/secret' => 'secrets#show'
+ root 'secrets#show'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  get '/show', to: 'secrets#show'
 end
